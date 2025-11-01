7a78c55a7add6b746865a6d7b7c58ef8e83328e5
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-01 11:39:59 +0900

    lazy.nvim: Change setup directory and installation procedure

--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
 -- lazy.nvim のセットアップ
-local lazypath = vim.fn.stdpath("config") .. "/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim", lazypath
+  })
+end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
