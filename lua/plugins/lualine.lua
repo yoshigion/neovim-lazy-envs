63a3014e9ff704b784b97330824e349676966dd9
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-03 21:07:00 +0900

    lualine: Correct indent

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,12 +2,12 @@
 -- status line
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require('lualine').setup({
+    require("lualine").setup({
       options = {
-	theme = 'auto',
-	globalstatus = true,
+        theme = "auto",
+        globalstatus = true,
       },
     })
   end,
