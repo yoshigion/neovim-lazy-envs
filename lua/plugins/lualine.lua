e9833d8f5d3ee43c6789ecd750a5b34b858fa1f3
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-11 21:46:23 +0900

    lua/plugins/lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "auto",
+        theme = "onedark",
         globalstatus = true,
       },
     })
