b06d2b07931f88ef947c9190dda0d941ef46831e
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-11 21:48:08 +0900

    lua/plugins/telescope.lua

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+-- lua/plugins/telescope.lua
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("telescope").setup({})
+  end
+}
