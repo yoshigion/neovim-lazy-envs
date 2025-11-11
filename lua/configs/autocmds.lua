b595936756b1c5c2bf6220692c34c29eae137d89
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-11 21:44:35 +0900

    lua/configs/autocmds.lua

---
+++ lua/configs/autocmds.lua
@@ -0,0 +1,11 @@
+-- lua/config/autocmds.lua
+--local group = vim.api.nvim_create_augroup("MyAutoCmds", { clear = true })
+
+-- 保存時に自動でフォーマット
+--vim.api.nvim_create_autocmd("BufWritePre", {
+--  group = group,
+--  pattern = "*",
+--  callback = function()
+--    vim.lsp.buf.format({ async = false })
+--  end,
+--})
