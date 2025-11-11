cbf8fa4c307676008ca1e0bb1d7890c92dd0a9ac
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-11 21:44:50 +0900

    lua/configs/keymaps.lua

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -6,6 +6,10 @@ local opts = { noremap = true, silent = true }
 -- Ctrl-e to toggle nvim-tree
 map("n", "<C-e>", ":NvimTreeToggle<CR>", opts)
 
+---- telescope.lua
+map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+map("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+
 ---- copilotchat.lua: avoid conflict <Tab>
 vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
 
