7191ec4b0794c5ff210b84d0c068854b42cc35f8
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-20 22:23:10 +0900

    plugins/mcphub.lua: Add MCP Hub

---
+++ lua/plugins/mcphub.lua
@@ -0,0 +1,12 @@
+-- lua/plugins/mcphub.lua
+-- MCP Client for Neovim
+return {
+  "ravitemer/mcphub.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+  },
+  build = "npm install -g mcp-hub@latest",
+  config =function()
+    require("mcphub").setup()
+  end,
+}
