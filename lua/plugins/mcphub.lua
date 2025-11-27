ddafbee66524d74ffae18e85b408e4f5fd2cd091
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-27 20:30:53 +0900

    plugins/mcphub.lua: Add comment of CopilotChat

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -11,8 +11,14 @@ return {
       extensions = {
         avante = {
           make_slash_commands = true, -- make /slash commands from MCP server prompts
-        }
+        },
+        --copilotchat = {
+        --  enabled = true,
+        --  convert_tools_to_functions = true,
+        --  convert_resources_to_functions = true,
+        --  add_mcp_prefix = false,
+        --}
       }
     })
-  end,
+  end
 }
