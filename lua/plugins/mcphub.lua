ef531fb62edcd169e1f1001546a210bc1e5a51f3
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-28 19:13:17 +0900

    plugins/mcphub.lua: Add integration of CopilotChat

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -13,12 +13,13 @@ return {
         avante = {
           make_slash_commands = true, -- make /slash commands from MCP server prompts
         },
-        --copilotchat = {
-        --  enabled = true,
-        --  convert_tools_to_functions = true,
-        --  convert_resources_to_functions = true,
-        --  add_mcp_prefix = false,
-        --}
+        -- https://ravitemer.github.io/mcphub.nvim/extensions/copilotchat.html#copilotchat-integration
+        copilotchat = {
+          enabled = true,
+          convert_tools_to_functions = true,     -- Convert MCP tools to CopilotChat functions
+          convert_resources_to_functions = true, -- Convert MCP resources to CopilotChat functions
+          add_mcp_prefix = false,                -- Add "mcp_" prefix to function names
+        },
       },
       -- https://ravitemer.github.io/mcphub.nvim/extensions/avante.html#global-auto-approval
       -- This sets vim.g.mcphub_auto_approve to true by default (can also be toggled from the HUB UI with `ga`)
