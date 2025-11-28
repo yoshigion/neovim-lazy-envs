d68ba8aded6fe734014b7646980c00b7de6988f1
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-28 19:14:28 +0900

    plugins/lualine.lua: Add comment about not integrate MCPHub

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,8 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("lualine").setup({
+      -- https://ravitemer.github.io/mcphub.nvim/extensions/lualine.html#lualine-integration
+      -- above description is incorrect, so not introduced
       options = {
         theme = "onedark",
         globalstatus = true,
