5701b0e4c3c90f0f6473f5b079d32a9182b02bf5
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-28 19:09:40 +0900

    plugins/mcphub.lua: Add comment and setting about URL information for Avante.nvim

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -9,6 +9,7 @@ return {
   config =function()
     require("mcphub").setup({
       extensions = {
+        -- https://ravitemer.github.io/mcphub.nvim/extensions/avante.html#configure-avante-integration
         avante = {
           make_slash_commands = true, -- make /slash commands from MCP server prompts
         },
@@ -18,7 +19,10 @@ return {
         --  convert_resources_to_functions = true,
         --  add_mcp_prefix = false,
         --}
-      }
+      },
+      -- https://ravitemer.github.io/mcphub.nvim/extensions/avante.html#global-auto-approval
+      -- This sets vim.g.mcphub_auto_approve to true by default (can also be toggled from the HUB UI with `ga`)
+      auto_approve = true,
     })
   end
 }
