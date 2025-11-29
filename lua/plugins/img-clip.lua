178b63c71f5b49fa27cd240818122724541cca25
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-11-30 08:36:01 +0900

    plugins/img-clip.lua: Add image clipboard paste

---
+++ lua/plugins/img-clip.lua
@@ -0,0 +1,27 @@
+-- lua/plugins/img-clip.lua
+return {
+  "HakonHarnes/img-clip.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add options here
+    -- or leave it empty to use the default settings
+  },
+  keys = {
+    -- suggested keymap
+    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+  },
+  default = {
+    prompt_for_file_name = false,
+    dir_path = "contents/assets",
+    file_name = function()
+      local current_buf_name_without_ext = vim.fn.expand("%:t:r")
+      return current_buf_name_without_ext .. "-%Y%m%d%H%M%S"
+    end,
+  },
+  config = function()
+    require("img-clip").setup({
+      dir_path = "contents/assets",
+      relateive_to_current_file = true,
+    })
+  end,
+}
