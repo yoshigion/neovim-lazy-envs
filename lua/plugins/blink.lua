9509cf1c1682f4eb420584d6dfaf8de66f336f76
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-12-02 22:55:15 +0900

    Revert "plugins/blink.lua: Add blink-cmp-avante integration"
    This reverts commit e9a6139674eed755a63a21bb0f168ea7bc187b54.
    
    blink plugin causes completion problems

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,8 +3,6 @@ return {
   "saghen/blink.cmp",
   -- optional: provides snippets for the snippet source
   dependencies = {
-    -- https://github.com/Kaiser-Yang/blink-cmp-avante#installation
-    { 'Kaiser-Yang/blink-cmp-avante' },
     { 'rafamadriz/friendly-snippets' },
   },
   -- use a release tag to download pre-built binaries
@@ -43,18 +41,7 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      -- Add 'avante' to the list: https://github.com/Kaiser-Yang/blink-cmp-avante?tab=readme-ov-file#installation
-      default = { 'avante', 'lsp', 'path', 'snippets', 'buffer' },
-      -- https://github.com/Kaiser-Yang/blink-cmp-avante?tab=readme-ov-file#installation
-      providers = {
-        avante = {
-          module = 'blink-cmp-avante',
-          name = 'Avante',
-          opts = {
-            -- options for blink-cmp-avante
-          }
-        }
-      },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
