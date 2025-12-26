2e5d4e5ec70f9c7848d570ccbad115d8ac2755aa
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2025-12-27 08:55:07 +0900

    configs/options.lua: Bump python version

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -3,7 +3,7 @@
 vim.opt.mouse = "" -- disable mouse
 
 -- specify python under pyenv
-vim.g.python3_host_prog = "~/.pyenv/versions/3.12.11/bin/python"
+vim.g.python3_host_prog = "~/.pyenv/versions/3.14.2/bin/python"
 -- specify ruby under rbenv
 vim.g.ruby_host_prog = "~/.rbenv/versions/3.4.7/bin/ruby"
 -- disable perl provider
