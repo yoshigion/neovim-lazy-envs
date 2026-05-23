e94e4bfaa22eb803fd043b25343b0ac942e9ab20
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2026-05-23 22:05:27 +0900

    lua/plugins/nvim-treesitter.lua: Disable because of embedded in neovim 0.12

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,32 +1,6 @@
 -- lua/plugins/nvim-treesitter.lua
--- tree sitter highlighting settings
+-- Neovim 0.12 では treesitter が組み込みになったため無効化
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "c",
-        "cpp",
-        "lua",
-        "python",
-        "javascript",
-        "html",
-        "css",
-        "json",
-        "bash",
-        "yaml",
-        "make",
-        "awk",
-      },
-      indent = {
-        enable = true,
-      },
-      highlight = {
-        enable = true,
-      },
-    })
-  end,
+  enabled = false,
 }
