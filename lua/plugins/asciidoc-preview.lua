13dc07741fd4cf71d4b6072c6bd5d6c044c8b718
Yoshimasa Yoshida <vangelis.albedo.0.39@gmail.com>
2026-05-26 23:56:19 +0900

    lua/plugins/asciidoc-preview.lua: Add preview function by edge on wsl2

--- lua/plugins/asciidoc-preview.lua
+++ lua/plugins/asciidoc-preview.lua
@@ -3,11 +3,39 @@
 return {
   "tigion/nvim-asciidoc-preview",
   ft = { "asciidoc" },
-  build = "cd server && npm install --omit=dev",
+  build = "cd server && npm install --omit=dev --no-save",
   ---@module 'asciidoc-preview'
   ---@type asciidoc-preview.Config
   opts = {
     -- Add user configuration here
+    server = {
+      -- Specifies how the AsciiDoc file is converted to HTML for the preview.
+      -- `js`  - asciidoctor.js (no local installation needed)
+      -- `cmd` - asciidoctor command (local installation needed)
+      converter = 'js',
+
+      -- Specifies the hostname or IP address of the preview website for the client.
+      -- This is only needed if you run neovim in a remote session and
+      -- want to access the preview website from another machine.
+      hostname = 'localhost',
+
+      -- Specifies the port of the preview website on the client and server side.
+      -- Must be between 10000 and 65535.
+      port = 11235,
+    },
+    preview = {
+      -- Specifies the scroll position of the preview website.
+      -- `current` - Keep current scroll position
+      -- `start`   - Start of the website
+      -- `sync`    - (experimental) Same (similar) position as in Neovim
+      --             => inaccurate, because very content dependent
+      position = 'current',
+
+      -- Use Asciidoctor configuration files (.asciidoctorconfig or .asciidoctorconfig.adoc).
+      -- The nearest configurations win over the farther ones.
+      -- This option is only relevant if the server converter is set to `js`.
+      use_asciidoctor_configs = false, ---@type boolean
+    },
   },
   config = function()
     vim.keymap.set("n", "<leader>ap", ":AsciiDocPreview<CR>", { noremap = true, silent = true })
